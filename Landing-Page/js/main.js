b96533ffd27aa9182ba244dc6196cff47c70688c
Surina Kate <surinak@fit.knu.ua>
2026-09-02 20:11:27 +0300

    1

--- Landing-Page/js/main.js
+++ Landing-Page/js/main.js
@@ -2,22 +2,21 @@ const countries = ["Ukraine", "USA", "Germany", "France", "Italy", "Spain", "Pol
 const locationInput = document.getElementById("location");
 const locationList = document.querySelector(".location-list");
 
-// відкриваємо список при фокусі
 locationInput.addEventListener("focus", () => {
-  locationList.innerHTML = ""; // очищаємо список
+  locationList.innerHTML = "";
   countries.forEach(country => {
     const li = document.createElement("li");
     li.textContent = country;
     li.addEventListener("click", () => {
-      locationInput.value = country; // вставляємо у інпут
-      locationList.style.display = "none"; // ховаємо список
+      locationInput.value = country;
+      locationList.style.display = "none";
     });
     locationList.appendChild(li);
   });
   locationList.style.display = "block";
 });
 
-// ховаємо список, якщо клік поза інпутом
+
 document.addEventListener("click", (e) => {
   if (!locationInput.contains(e.target) && !locationList.contains(e.target)) {
     locationList.style.display = "none";
